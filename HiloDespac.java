26a030d83112d9d76053eeb447952c09e1a05ffd
RadW2020 <RadW2020@users.noreply.github.com>
2017-02-28 15:06:49 +0100

    Add files via upload

---
+++ HiloDespac.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaquetePrincipal;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ *
+ * @author RadW
+ */
+class HiloDespachador extends Thread {
+
+    private Socket socketCliente;
+
+    public HiloDespachador(Socket socketCliente) {
+
+        this.socketCliente = socketCliente;
+
+    }
+
+  public void run() {
+
+    try{
+      
+        ServidorHTTP.procesaPeticion(socketCliente);
+        //cierra la conexión entrante
+        socketCliente.close();
+        System.out.println("cliente atendido");
+          
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+    
+  }
+
+}
